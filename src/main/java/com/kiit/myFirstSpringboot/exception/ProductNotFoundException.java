0cdcc9e596d1686f5a63311e864a4262490dd1a0
KIIT <KIIT@10.0.137.79>
2025-07-23 23:59:02 +0530

    Initial Commit - springboot-product

--- src/main/java/com/kiit/myFirstSpringboot/exception/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.kiit.myFirstSpringboot.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-	public ProductNotFoundException(String errMsg)
-	{
-	 super(errMsg);
-	}
-}
