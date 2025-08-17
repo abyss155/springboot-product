751cfd6609200ef54828c689bef376d3f1c844bd
Pankaj Soni <ps4551442@gmail.com>
2025-08-17 23:38:19 +0530

    some updates

--- src/main/java/com/kiit/myFirstSpringboot/model/User.java
+++ src/main/java/com/kiit/myFirstSpringboot/model/User.java
@@ -9,10 +9,9 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.JoinColumn;
-
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
